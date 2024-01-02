d542a0ad2da6b224adfd6d4ed2225f5f4ae4daf6
ofirlevy6658 <ofirlevy6658@gmail.com>
2024-01-02 18:48:32 +0200

    commandFileHandler

--- file-system/app.js
+++ file-system/app.js
@@ -1,14 +1,31 @@
-const fs = require("fs");
-const fsPromises = require("fs").promises;
+const { watch, open } = require("node:fs/promises");
 
-const content = fs.readFileSync("./file.txt", "utf8");
-console.log(content);
+const ac = new AbortController();
+const { signal } = ac;
+setTimeout(() => ac.abort(), 10000);
 
 (async () => {
   try {
-    await fsPromises.copyFile("./file.txt", "./file-copy.txt");
-    console.log("file copied");
-  } catch (error) {
-    console.log(error);
+    const commandFileHandler = await open("./command.txt", "r");
+    const watcher = watch("./command.txt", { signal });
+    for await (const event of watcher) {
+      if (event.eventType === "change") {
+        const { size } = await commandFileHandler.stat();
+        const buff = Buffer.alloc(size);
+        const offset = 0;
+        const length = buff.byteLength;
+        const position = 0;
+        const content = await commandFileHandler.read(
+          buff,
+          offset,
+          length,
+          position
+        );
+        console.log(content);
+      }
+    }
+  } catch (err) {
+    if (err.name === "AbortError") return;
+    throw err;
   }
 })();
